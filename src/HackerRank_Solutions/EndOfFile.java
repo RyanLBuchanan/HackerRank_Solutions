2b2557051f53bfdf6a549770337345fadeaaa39e
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-03-02 12:35:12 -0700

    feat: Completed End Of File tutorial

---
+++ src/HackerRank_Solutions/EndOfFile.java
@@ -0,0 +1,23 @@
+package HackerRank_Solutions;
+import java.io.*;
+import java.util.*;
+
+
+public class EndOfFile {
+	
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        while(scan.hasNextLine()){
+        	String str=scan.nextLine();
+            	if (str.contains("end-of-file")){
+            		i++;
+            		System.out.println(i+" "+str);
+            		break;
+            	}else{
+            		i++;
+            		System.out.println(i+" " +str);       
+            }
+        }  
+    }
+}
