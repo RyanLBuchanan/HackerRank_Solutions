f361671b4bb938971b91edbd01d2a39b3e9a831f
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-03-03 08:40:27 -0700

    feat: Completed Welcome to Java tutorial

--- src/HackerRank_Solutions/EndOfFile.java
+++ src/HackerRank_Solutions/EndOfFile.java
@@ -9,14 +9,15 @@ public class EndOfFile {
         Scanner scan = new Scanner(System.in);
         int i = 0;
         while(scan.hasNextLine()){
-        	String str=scan.nextLine();
-            	if (str.contains("end-of-file")){
-            		i++;
-            		System.out.println(i+" "+str);
-            		break;
-            	}else{
-            		i++;
-            		System.out.println(i+" " +str);       
+        	String str = scan.nextLine();
+        	
+        	if (str.contains("end-of-file")){
+        		i++;
+        		System.out.println(i+" "+str);
+        		break;
+        	}else{
+        		i++;
+        		System.out.println(i+" " +str);       
             }
         }  
     }
---
+++ src/HackerRank_Solutions/WelcomeToJava.java
@@ -0,0 +1,9 @@
+package HackerRank_Solutions;
+
+public class WelcomeToJava {
+	public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+	}
+
+}
